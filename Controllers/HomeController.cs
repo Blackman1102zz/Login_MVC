454d5d73ce06370b08c8ffaf9bba1f77639a96db
vanh <blackman1102zz@gmail.com>
2022-09-25 14:03:21 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Data_Grid.Models;
+using System.Data.SqlClient;
+namespace Data_Grid.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            con.ConnectionString = Data_Grid.Properties.Resources.ConnectionString;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Verify(Account acc)
+
+        {
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Login where Taikhoan ='"+acc.Name+"' and Matkhau='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
